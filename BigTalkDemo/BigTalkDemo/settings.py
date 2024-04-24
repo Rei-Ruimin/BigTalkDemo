9e905de555f1d28cbdb3e99d725435b9beafde5b
Rei <60533945+Rei-Ruimin@users.noreply.github.com>
2024-04-24 18:51:46 -0400

    change settings.py to production

--- BigTalkDemo/BigTalkDemo/settings.py
+++ BigTalkDemo/BigTalkDemo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jlk6c@on+)+m!k^&)e(+uf6@xzr(3kpm8^sea%&b%3&530tupg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
