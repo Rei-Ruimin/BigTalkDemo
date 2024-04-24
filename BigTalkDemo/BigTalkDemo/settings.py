b9b31aafa53c06660e3c05dbe20e08661c732250
Rei <60533945+Rei-Ruimin@users.noreply.github.com>
2024-04-24 14:03:23 -0400

    v1

--- BigTalkDemo/BigTalkDemo/settings.py
+++ BigTalkDemo/BigTalkDemo/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'one_page_demo/static'),
 ]
+
 
 
 # Default primary key field type
