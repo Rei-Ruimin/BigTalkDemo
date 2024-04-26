0c18c91895ea1d3b4f7566b74e8c3c297094b6ad
Rei <60533945+Rei-Ruimin@users.noreply.github.com>
2024-04-26 00:42:33 -0400

    work with audio

--- BigTalkDemo/one_page_demo/views.py
+++ BigTalkDemo/one_page_demo/views.py
@@ -1,23 +1,45 @@
+from time import sleep
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+import requests
 
+
+DEEPGRAM_API_KEY = '17e4f14bc5e82df0ece99c45eec4755855b27860'
 def home(request):
     return render(request, 'home.html')
 
 def result(request):
     return render(request, 'result.html')
 
-
-def upload_video(request):
+def handle_video(request):
     if request.method == 'POST':
-        video_file = request.FILES['video']
-        # Process video, analyze, etc.
-        result = process_video(video_file)
+        video_file = request.FILES.get('videoFile')
+        audio_file = request.FILES.get('audioFile')
+        if video_file and audio_file:
+            # Process the video and audio files
+            response_data = process_media(video_file, audio_file)
+            return render(request, 'result.html', {'data': response_data})
+        else:
+            return HttpResponse("No video or audio file uploaded.", status=400)
+    return HttpResponse("Invalid request", status=400)
 
-        # Store result in session
-        request.session['video_result'] = result
+def process_media(video_file, audio_file):
+    # Prepare the result dictionary
+    result = {'video_status': f'Processed {video_file.name}'}
+    url = "https://api.deepgram.com/v1/listen?filler_words=true"
+    headers = {
+        "Authorization": 'Token 17e4f14bc5e82df0ece99c45eec4755855b27860',  # Replace DEEPGRAM_API_KEY with your actual API key
+        "Content-Type": "audio/*"
+    }
 
-        return JsonResponse({'success': True})
+    audio_file = audio_file.read()
 
-def result_page(request):
-    result = request.session.get('video_result')
-    return render(request, 'result.html', {'result': result})
+    # Send the audio file to Deepgram
+    response = requests.post(url, headers=headers, data=audio_file)
+
+    result['audio_status'] = response.json()
+    # Return the JSON response from Deepgram to the frontend
+    return result
+    
+
